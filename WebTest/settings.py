3a67f1b78fd1038e78028544bfb5acfdbd80a2d3
Roy <roypaul.narido@g.msuiit.edu.ph>
2023-10-25 13:37:14 +0800

    deploy website test

--- WebTest/settings.py
+++ WebTest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-n18l**o8%+5h&^r6pmqo#*4a8g7@ze324%!ja*oqo+k^-&vz^&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
